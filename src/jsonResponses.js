85f2d05e2e14824fc65eee756fb1eac6256812ad
OliverH22 <42745916+OliverH22@users.noreply.github.com>
2021-03-08 14:45:57 -0500

    Add files via upload

---
+++ src/jsonResponses.js
@@ -0,0 +1,111 @@
+const userClosets = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+const success = (request, response) => {
+  const responseJSON = {
+    message: 'This is a successful response',
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+
+const badRequest = (request, response) => {
+  const responseJSON = {
+    message: 'This request has the required parameters',
+  };
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const getUserClosets = (request, response) => {
+  // should default to good
+  const responseJSON = {
+    userClosets,
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+const getUserClosetsMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+
+const addUserItem = (request, response, body) => {
+  const responseJSON = {
+    message: 'image, name, and cost are required!!',
+  };
+
+  if (!body.name || !body.cost || !body.category || !body.url) {
+    respondJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  // item name does it all i guess
+  if (userClosets[body.name]) {
+    responseCode = 204;
+  } else {
+    userClosets[body.name] = {};
+  }
+
+  userClosets[body.name].name = body.name;
+  userClosets[body.name].cost = body.cost;
+  userClosets[body.name].category = body.category;
+  userClosets[body.name].url = body.url;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'created successfully!';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  console.log('added');
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const addItemMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+
+const notReal = (request, response) => {
+  // should default to good
+  const responseJSON = {
+    message: 'This was successfully not found',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+
+module.exports = {
+  success,
+  badRequest,
+  notFound,
+  getUserClosets,
+  getUserClosetsMeta,
+  addUserItem,
+  addItemMeta,
+  notReal,
+};
--- src/server.js
+++ src/server.js
@@ -4,6 +4,62 @@ const mediaHandler = require('./mediaResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/success': jsonHandler.success,
+    '/badRequest': jsonHandler.badRequest,
+    '/getUserClosets': jsonHandler.getUserClosets,
+    '/addUserItem': jsonHandler.addUserItem,
+    '/notReal': jsonHandler.notReal,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    // these should all be meta ones! bc no message :)
+    '/getUserClosetsMeta': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notRealMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};// Change types
+
+const handlePost = (request, response) => {
+  const res = response;
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    res.statusCode = 400;
+    res.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    jsonHandler.addUserItem(request, res, bodyParams);
+  });
+};
+
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  //console.log(params);
+
+  if (request.method === 'POST' && parsedUrl.pathname === '/addUserItem') {
+    handlePost(request, response, parsedUrl);
+  } else if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct.GET.notFound(request, response);
+  }
+};
+
+////////////////////////////////////////////////////////
 const onRequest = (request, response) => {
   console.log(request.url);
 
